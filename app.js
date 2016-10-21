ed0c52ad4942a72b45c3e053392a8c644383a1d2
narendranath1150 <narendranath.mca@gmail.com>
2016-10-21 13:48:04 +0530

    New

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ app.get('/webhook', function(req, res) {
     //console.log("Validating webhook", console.log(JSON.stringify(req.body)));
     console.log("######################################", res);
     if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === 'hrlogin_type') {
-          //req.query['hub.verify_token'] === 'login_type') {
+        //req.query['hub.verify_token'] === 'hrlogin_type') {
+        req.query['hub.verify_token'] === 'login_type') {
         res.status(200).send(req.query['hub.challenge']);
     } else {
         console.error("Failed validation. Make sure the validation tokens match.");
